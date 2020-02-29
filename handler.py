686db1d3318b3bbf521fb89c45936ae53dca96c3
nalbam <me@nalbam.com>
2020-02-29 20:38:36 +0900

    0.003

--- handler.py
+++ handler.py
@@ -90,7 +90,7 @@ def make_rectangle(src_key, dst_key, box):
 
     left, top, right, bottom = get_bounding_box(width, height, box)
 
-    line_width = int(width * 0.003)
+    line_width = max(int(width * 0.003), 1)
 
     cv2.rectangle(src, (left, top), (right, bottom), LINE_COLOR, line_width)
 
